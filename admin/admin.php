3f3a951d1c0a208c35be0690f3b747109c30bd8b
daseul <nldaseul@gmail.com>
2019-04-05 21:33:04 +0900

    admin.php(쿠폰발행페이지) 추가

---
+++ admin/admin.php
@@ -0,0 +1,32 @@
+<?php include('../functions.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin - 쿠폰 코드 발행 페이지</title>
+</head>
+<body>
+	<h2>Admin - 쿠폰 코드 발행 페이지</h2>
+	<!-- 알림 메세지 -->
+	<?php if (isset($_SESSION['success'])) : ?>
+		<h3>
+			<?php
+				echo $_SESSION['success'];
+				unset($_SESSION['success']);
+			?>
+		</h3>
+	<?php endif ?>
+
+	<!-- admin user 정보 -->
+	<?php  if (isset($_SESSION['user'])) : ?>
+		<strong><?php echo $_SESSION['user']['username']; ?></strong>
+
+		<small>
+			<i style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i>
+			<br>
+			<a href="admin.php?logout='1'" style="color: red;">logout</a>
+      <a href="create_user.php"> add user</a>
+		</small>
+
+	<?php endif ?>
+</body>
+</html>
