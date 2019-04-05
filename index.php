9a7c733b1f3b60915dd92eff1c28fe8cb9f273b9
daseul <nldaseul@gmail.com>
2019-04-05 21:15:31 +0900

    index.php(쿠폰사용페이지)에 user 프로필 추가

--- index.php
+++ index.php
@@ -14,4 +14,17 @@
 				unset($_SESSION['success']);
 			?>
 		</h3>
-    
+	<?php endif ?>
+	<!-- 로그인한 user 정보 -->
+  <?php if (isset($_SESSION['user'])) : ?>
+  	<strong><?php echo $_SESSION['user']['username']; ?></strong>
+
+  	<small>
+  		<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i>
+  		<br>
+  		<a href="index.php?logout='1'" style="color: red;">logout</a>
+  	</small>
+
+  <?php endif ?>
+</body>
+</html>
